0fe1e3e58307fe6b6fa848880d1a1a2fffde18e1
Dev <amicable026@gmail.com>
2024-05-28 00:41:07 -0400

    calculate posterior

--- starter.py
+++ starter.py
@@ -15,11 +15,36 @@ def calculate_priors(emails):
     prior_ham = 1 - prior_spam
     return prior_spam, prior_ham
 
-def calculate_posterior(emails):
-    """This function should return the probability if an email contains the
-    word 'lottery.'
-    """
-    pass
+def calculate_posterior(emails, given_words):
+    words_in_spam = {}
+    words_in_ham = {}
+    for _, email in emails.iterrows(): #
+        if email["spam"] == 1:
+            for word in email["words"]:
+                words_in_spam[word] = words_in_spam.get(word, 0) + 1
+        else:
+            for word in email["words"]:
+                words_in_ham[word] = words_in_ham.get(word, 0) + 1
+    prior_spam, prior_ham = calculate_priors(emails)
+    total_spam_word_count = sum(words_in_spam.values())
+    total_ham_word_count = sum(words_in_ham.values())
+    
+    probability_of_word_given_spam = 1 # initially
+    for given_word in given_words:
+        count_given_word_in_spam = words_in_spam.get(given_word, 0)
+        probability_of_word_given_spam *= (count_given_word_in_spam + 1) / (total_spam_word_count + len(words_in_spam))
+        # every word will affect the probability
+
+    probability_of_word_given_ham = 1
+    for given_word in given_words:
+        count_given_word_in_ham = words_in_ham.get(given_word, 0)
+        probability_of_word_given_ham *= (count_given_word_in_ham + 1) / (total_ham_word_count + len(words_in_ham))
+
+    probability_of_observing_given_word = (probability_of_word_given_spam * prior_spam) + (probability_of_word_given_ham * prior_ham)
+
+    posterior = (probability_of_word_given_spam * prior_spam) / probability_of_observing_given_word
+    return posterior
+
     
 
 emails["words"] = emails["text"].apply(process_email)
